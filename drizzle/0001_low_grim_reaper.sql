9ee3ef530c476dfa8fcef448b19e91089f9eba00
letnull19A <alexvolkovdd@googlemail.com>
2025-11-20 15:21:58 +0400

    refactor: add conditional check for unique constraint on help_types table to prevent migration errors

--- drizzle/0001_low_grim_reaper.sql
+++ drizzle/0001_low_grim_reaper.sql
@@ -1,1 +1,11 @@
-ALTER TABLE "help_types" ADD CONSTRAINT "help_types_name_unique" UNIQUE("name");
+DO $$ BEGIN
+ IF NOT EXISTS (
+  SELECT 1 FROM information_schema.table_constraints
+  WHERE constraint_name = 'help_types_name_unique'
+   AND table_name = 'help_types'
+ ) THEN
+  ALTER TABLE "help_types" ADD CONSTRAINT "help_types_name_unique" UNIQUE("name");
+ END IF;
+EXCEPTION
+ WHEN duplicate_object THEN NULL;
+END $$;
