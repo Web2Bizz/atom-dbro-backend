5c0a3f7330929d25fba42c4fab3cf985b803d511
letnull19A <alexvolkovdd@googlemail.com>
2025-11-26 00:51:08 +0400

    refactor: remove RabbitMQController to enhance code clarity and maintainability

--- src/rabbitmq/rabbitmq.module.ts
+++ src/rabbitmq/rabbitmq.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RabbitMQService } from './rabbitmq.service';
-import { RabbitMQController } from './rabbitmq.controller';
 
 @Module({
   providers: [RabbitMQService],
-  controllers: [RabbitMQController],
+  controllers: [],
   exports: [RabbitMQService],
 })
 export class RabbitMQModule {}
