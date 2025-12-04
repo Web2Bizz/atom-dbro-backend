60aedc285aea95408847f37c4e2bcd103e6d89ad
letnull19A <alexvolkovdd@googlemail.com>
2025-12-04 18:36:30 +0400

    feat: implement AchievementEventsService and AchievementEventsHandler to manage achievement awarded events and user experience rewards

--- src/achievement/achievement.module.ts
+++ src/achievement/achievement.module.ts
@@ -2,13 +2,21 @@ import { Module } from '@nestjs/common';
 import { AchievementService } from './achievement.service';
 import { AchievementController } from './achievement.controller';
 import { AchievementRepository } from './achievement.repository';
+import { AchievementEventsService } from './achievement.events';
+import { AchievementEventsHandler } from './achievement-events.handler';
 import { DatabaseModule } from '../database/database.module';
+import { ExperienceModule } from '../experience/experience.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ExperienceModule],
   controllers: [AchievementController],
-  providers: [AchievementService, AchievementRepository],
-  exports: [AchievementService],
+  providers: [
+    AchievementService,
+    AchievementRepository,
+    AchievementEventsService,
+    AchievementEventsHandler,
+  ],
+  exports: [AchievementService, AchievementEventsService],
 })
 export class AchievementModule {}
 
